c73a156a2718746b534770379390c02062a6b701
Alexander Mann-Wahrenberg <Alexander.Mann-Wahrenberg@continental-corporation.com>
2016-06-28 19:51:13 +0200

    Backup

--- H_Servo/H_Servo.ino
+++ H_Servo/H_Servo.ino
@@ -1,22 +1,16 @@
+#include <Servo.h>
+
+Servo servo;
+
 void setup(){
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
+   servo.attach(5);
 }
 
 void loop() {
-  int x = analogRead(0);
-  Serial.println(x);
-
-
-  if(x > 1020){
-    digitalWrite(2, LOW);
-    digitalWrite(3, HIGH);
-  }
-
-  if(x < 900){
-    digitalWrite(2, HIGH);
-    digitalWrite(3, LOW);
-  }
-
+  servo.write(50);
+  delay(1000);
+  servo.write(100);
+  delay(1000);
+  servo.write(150);
+  delay(1000);
 }
---
+++ J_Lautsprecher/J_Lautsprecher.ino
@@ -0,0 +1,10 @@
+void setup(){
+   pinMode(5, OUTPUT);
+}
+
+void loop() {
+   tone(5, 440);
+   delay(1000);
+   noTone(5);
+   delay(500);
+}
