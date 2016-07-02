0f68e1ed996b4e2d46084b5865f398e94363c407
Alexander Mann-Wahrenberg <Alexander.Mann-Wahrenberg@continental-corporation.com>
2016-07-02 19:58:58 +0200

    Blaue Widerstände und exportierte Bilder

--- C_LEDanDigitalemPin/C_LEDanDigitalemPin.ino
+++ C_LEDanDigitalemPin/C_LEDanDigitalemPin.ino
@@ -1,8 +1,10 @@
 void setup() {
-   pinMode(2, OUTPUT);
-   digitalWrite(2, HIGH);
+  pinMode(3, OUTPUT);
 }
 
 void loop() {
-  
+   digitalWrite(3, HIGH);
+   delay(2000);
+   digitalWrite(3, LOW);
+   delay(2000);
 }
