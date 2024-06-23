11af37796dfb9b767c4ddedad1fc4e268a20ed4b
aman <aman@vitronic.com>
2024-06-23 16:38:58 +0200

    Use (parts of) most recent hermesbaby docs-as-code environment

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+### Enable exit on error ######################################################
+set -e
+
+### Update local apt index ####################################################
+
+sudo apt-get update -y
+
+### Make python3 available under name "python" ################################
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get install -y   \
+    python-is-python3
+
+### Install pip  ##############################################################
+
+curl -sS https://bootstrap.pypa.io/get-pip.py | python
+
+
+### Install venv  #############################################################
+
+sudo apt-get install -y python3.10-venv
+
+
+### Install poetry inside pipx ################################################
+sudo python -m pip install pipx
+sudo python -m pipx install poetry
+sudo pipx ensurepath
+export PATH="$PATH:$HOME/.local/bin" # Works at least for root. That's fine for docker/devcontainers
+
+
+### Make available drawio in headless mode ####################################
+
+# Install drawio
+
+if which drawio; then
+    echo "drawio is already installed"
+else
+    drawio_package=drawio-amd64-24.4.13.deb
+    sudo curl -L -o $drawio_package https://github.com/jgraph/drawio-desktop/releases/download/v24.4.13/$drawio_package
+    sudo apt install -y ./$drawio_package
+    rm $drawio_package
+fi
+
+# Install virtual X-Server
+sudo apt-get install -y xvfb
+
+### EOF #######################################################################
+
+
---
+++ docs/conf_project.py
@@ -0,0 +1,14 @@
+### PROJECT CONFIGURATION #####################################################
+title    = "Willi-Makers"
+author   = "Alexander Mann-Wahrenberg (basejumpa)"
+
+project              = "Willi-Makers"
+scm_repo_url_git     = "https://github.com/basejumpa/willi-makers.git"
+scm_repo_url_browse  = "https://github.com/basejumpa/willi-makers"
+
+
+# color_primary = "indigo"
+# Options are red, pink, purple, deep-purple, indigo, blue, light-blue, cyan, teal, green, light-green, lime, yellow, amber, orange, deep-orange, brown, grey, blue-grey, and white.
+
+
+### EOF #######################################################################
